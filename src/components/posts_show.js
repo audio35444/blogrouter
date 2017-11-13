5c4ce638756cc6d2d2b9d8d0eaffae399232a460
dev3 <dev3@dev2s-MacBook-6.local>
2017-11-13 17:53:00 -0300

    2docommit

---
+++ src/components/posts_show.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class PostShow extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>Posts Show!</div>
+    );
+  }
+}
+
+export default PostShow;
--- src/index.js
+++ src/index.js
@@ -2,33 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
-import App from './components/app';
+//import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-class Hello extends React.Component{
-  render(){
-    return(
-      <div>Hello!</div>
-    );
-  }
-}
-class Goodbye extends React.Component{
-  render(){
-    return(
-      <div>Goodbye!</div>
-    );
-  }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+// class Hello extends React.Component{
+//   render(){
+//     return(
+//       <div>Hello!</div>
+//     );
+//   }
+// }
+// class Goodbye extends React.Component{
+//   render(){
+//     return(
+//       <div>Goodbye!</div>
+//     );
+//   }
+// }
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <h1>Header</h1>
-      <Route path="/hello" component={Hello}/>
-      <Route path="/goodbye" component={Goodbye}/>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew}/>
+          <Route path="/posts/:id" component={PostsShow}/>
+          <Route path="/" component={PostsIndex}/>
+        </Switch>
     </div>
     </BrowserRouter>
   </Provider>
